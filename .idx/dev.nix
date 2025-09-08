ca153be7ec201c3642eca9f962761597bad092ad
edisoncint06 <edisoncint06@gmail.com>
2025-09-08 19:31:45 +0000

    Initialized workspace

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,23 +1,22 @@
-# To learn more about how to use Nix to configure your environment
-# see: https://developers.google.com/idx/guides/customize-idx-env
 { pkgs, ... }: {
-  # Which nixpkgs channel to use.
-  channel = "stable-24.05"; # or "unstable"
-  # Use https://search.nixos.org/packages to find packages
-  packages = [ pkgs.python3 ];
+  channel = "stable-24.05";
+  packages = [
+    pkgs.python3
+    pkgs.python3Packages.pip
+  ];
   idx = {
-    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [ "ms-python.python" ];
     workspace = {
-      # Runs when a workspace is first created with this `dev.nix` file
-      onCreate = {
-        install =
-          "python -m venv .venv && source .venv/bin/activate && pip install -r requirements.txt";
-        # Open editors for the following files by default, if they exist:
-        default.openFiles = [ "README.md" "src/index.html" "main.py" ];
-      }; # To run something each time the workspace is (re)started, use the `onStart` hook
+      onStart = {
+  setup = ''
+    [ ! -d .venv ] && python -m venv .venv
+    source .venv/bin/activate
+    pip install --upgrade pip
+    pip install qrcode pillow flask-socketio flask-sqlalchemy
+  '';
+};
+
     };
-    # Enable previews and customize configuration
     previews = {
       enable = true;
       previews = {
--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import os
 
-from flask import Flask, send_file
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return send_file('src/index.html')
+@app.route("/payments/pix", methods=['POST'])
+def create_payments_pix():
+    return jsonify({'message': 'Pix payment created'}), 201
+
+@app.route("/payments/pix/confirmation", methods=['POST'])
+def pix_confirmation():
+    return jsonify({'message': 'Pix payment confirmed'}), 201
+
+#webhook
+@app.route('/payments/pix/<int:payment_id>', methods=['GET']) 
+def payments_pix_page(payment_id):
+    return 'payment pix'
 
 def main():
     app.run(port=int(os.environ.get('PORT', 80)))
 
 if __name__ == "__main__":
-    main()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
