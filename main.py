b4329ba415339e4ac5671c0d2c4e05a6f237b94d
edisoncint06 <edisoncint06@gmail.com>
2025-09-10 11:36:54 +0000

    Initialized Workspace

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import os
-
+from repository.database import db 
 from flask import Flask, jsonify, request
+from db_models.payment import Payment
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'SECRET_KEY_WEBSOCKET'
+
+db.init_app(app)
+
 
 @app.route("/payments/pix", methods=['POST'])
 def create_payments_pix():
---
+++ shell.py
@@ -0,0 +1,8 @@
+from repository.database import db
+from main import app
+
+with app.app_context():
+    db.create_all()  
+    print('BD CRIADO')   
+    
+   
